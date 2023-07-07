fcd1deafe60c0ed52b18cf91b87c5671bd84999f
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-07-06 22:24:39 -0700

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,69 @@
-export * from '@/sbstr8/components/layout/root';
-import Default from '@/sbstr8/components/layout/root';
-export default Default;
+import urlJoin from 'url-join';
+import '@/sbstr8/lib/style.css';
+import pkg from '@/../package.json';
+import cfg, { defaultAuthor } from '@/../sbstr8.config';
+import defaults from '@/sbstr8/lib/default';
+import './theme.css';
+
+const MainLayout = ({ children }: React.PropsWithChildren<unknown>) => (
+  <html lang="en" className="s8-layout-root">
+    <body>
+      {children}
+      <footer className="s8-layout-root-footer">
+        <h6>
+          &copy; {cfg.copyright}
+          &nbsp; Built with{' '}
+          <a href="https://github.com/elmarsto/sbstr8">sbstr8</a>.
+        </h6>
+      </footer>
+    </body>
+  </html>
+);
+export default MainLayout;
+
+export const metadata = {
+  applicationName: cfg.title,
+  authors: cfg.owners.map(({ name, link }) => ({ name, url: link })),
+  alternates: {
+    canonical: cfg.link,
+    types: {
+      'application/rss+xml': urlJoin(cfg.link, defaults.path.feed),
+    },
+  },
+  category: cfg.categories.join('/'),
+  creator: defaultAuthor.name,
+  description: cfg.description,
+  generator: pkg.name,
+  keywords: cfg.keywords,
+  metadataBase: new URL(cfg.link),
+  openGraph: {
+    description: cfg.description,
+    locale: cfg.language,
+    siteName: cfg.title,
+    title: cfg.title,
+    type: 'website',
+    url: '/',
+    images: cfg.image
+      ? [
+          {
+            url: urlJoin(cfg.link, cfg.image),
+          },
+        ]
+      : [],
+  },
+  publisher: pkg.author,
+  robots: {
+    follow: false, // don't follow; instead use @/app/sitemap.ts
+    googleBot: {
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+    index: true,
+  },
+  title: cfg.title,
+  icons: {
+    icon: cfg.icon,
+    shortcut: cfg.icon,
+    apple: cfg.icon,
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,48 @@
-import rootMaker from '@/sbstr8/components/page/root';
-import FirstPost from '@/app/posts/first-post';
+import { ReactNode } from 'react';
+import { gql } from '@apollo/client';
+import ccn from '@sindresorhus/class-names';
+import cfg from '@/../sbstr8.config';
+import ReadMore from '@/sbstr8/components/read-more';
+import Feature from '@/sbstr8/components/feature';
+import Image from '@/sbstr8/components/image';
+import { PageHeader } from '@/sbstr8/components/page/header';
+import { Post } from '@/sbstr8/lib/types/post';
+import LedeList from '@/sbstr8/components/lede-list';
+import { sClient } from '@/sbstr8/lib/graphql/server';
 
-export default rootMaker({
-  primary: [{ post: FirstPost, link: '/posts/first-post' }],
-});
+const LOGO_SZ = 32;
+
+const defaultPostPath = '/posts/';
+const defaultLogo = '/media/sbstr8.svg';
+const query = gql`
+  query {
+    lastModified
+    posts {
+      date
+      description
+      image
+      thumbnail
+      slug
+      title
+    }
+  }
+`;
+
+const Root = async () => {
+  const { data } = await sClient.query({ query });
+  const logo = cfg.icon || defaultLogo;
+  return (
+    <div className="s8-page-root">
+      <PageHeader className="s8-page-root-header">
+        <Image src={logo} width={LOGO_SZ} height={LOGO_SZ} alt="logo" />
+      </PageHeader>
+      <main className="s8-page-root-main">
+        <section className="s8-page-root-section-unfeatured">
+          <LedeList>{data.posts}</LedeList>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Root;
