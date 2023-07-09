6a8a8ea07322c5843738d82a1b8b495425166c23
Elizabeth Marston <315987+elmarsto@users.noreply.github.com>
2023-07-09 14:06:23 -0700

    config fix

--- src/app/news/first-post/page.tsx
+++ src/app/news/first-post/page.tsx
@@ -6,7 +6,6 @@ import Standard from '@/sbstr8/components/post/standard';
 const Page = async () => (
   <Standard
     created={meta['created']}
-    image={meta['image']}
     title={meta['title']}
     updated={(meta as IPost)['updated']}
     footnotes={<Post.Footnotes />}
